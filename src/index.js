fe87054f22dd253b91225d73710c97c431bd2d2a
Raynecloudss <rayne.a.fall@gmail.com>
2023-11-19 21:12:16 -0500

    functions added to java
    multiple functions added to js for city and current temp display (real time)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "a38c4204at848f00bec4c7fabo002e83";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
