715f89320d09e15d02b021c94334a905f1c5ef37
Raynecloudss <rayne.a.fall@gmail.com>
2023-12-03 19:48:47 -0500

    Call function changed

--- src/index.js
+++ src/index.js
@@ -88,5 +88,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-displayForecast();
-getForecast();
+getForecast(response.data.city);
