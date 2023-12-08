3947b8d1090d2718e312e92da66c27e596cae280
Raynecloudss <rayne.a.fall@gmail.com>
2023-12-07 20:28:51 -0500

    Function added and weather icon changed

--- src/index.js
+++ src/index.js
@@ -53,6 +53,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "a38c4204at848f00bec4c7fabo002e83";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -60,16 +67,16 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
         <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">☀️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+<img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temps">
         <div class="weather-forecast-temp">
         <strong>${Math.round(day.temperature.maximum)}°</strong>
@@ -80,6 +87,7 @@ function displayForecast(response) {
         </div>
         </div>
         `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
